1db471a80bdfeea5910a954f17f40ae2c59ced6c
Alexandr Birichevskiy <alexandrbirichevskiy@gmail.com>
2021-02-16 20:40:50 +0300

    homework9 v1.1

--- Homework_9/src/main/java/com/alexbirichevskiy/homework_9/Main.java
+++ Homework_9/src/main/java/com/alexbirichevskiy/homework_9/Main.java
@@ -6,34 +6,53 @@ public class Main {
         String[][] arr = {
                 {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"},
+                {"1", "2", "3", "2"},
                 {"1", "2", "3", "4"},
-                {"1", "2", "3", "4"}
         };
 
         try {
-            twoDimensionalArray(arr);
+            System.out.println(twoDimensionalArray(arr));
         } catch (MyArraySizeException e) {
             System.out.println("Исключение: " + e);
+        } catch (MyArrayDataException e) {
+            System.out.println("Исключениe: " +e);
         }
     }
 
-    public static void twoDimensionalArray (String[][] arr) throws MyArraySizeException {
+    public static int twoDimensionalArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
         int numLine = 0;
         int numCol;
+        int xCoord = 0;
+        int yCoord = 0;
+
         if (arr.length != 4) {
             throw new MyArraySizeException(arr.length);
-        }
-        else {
+        } else {
             for (String[] line : arr) {
-                numLine ++;
+                numLine++;
                 numCol = 0;
                 for (int i = 0; i < line.length; i++) {
-                    numCol ++;
+                    numCol++;
                 }
                 if (numCol != 4) {
-                    throw new MyArraySizeException(arr.length, numLine, numCol);
+                    throw new MyArraySizeException(arr.length, numLine-1, numCol);
                 }
             }
         }
+
+
+        for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    try {
+                        xCoord = i;
+                        yCoord = j;
+                        sum += Integer.parseInt(arr[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException(xCoord, yCoord);
+                    }
+                }
+        }
+        return sum;
     }
 }
--- Homework_9/src/main/java/com/alexbirichevskiy/homework_9/MyArrayDataException.java
+++ Homework_9/src/main/java/com/alexbirichevskiy/homework_9/MyArrayDataException.java
@@ -1,4 +1,16 @@
 package com.alexbirichevskiy.homework_9;
 
-public class MyArrayDataException extends Exception{
+public class MyArrayDataException extends Exception {
+    private int x;
+    private int y;
+
+    MyArrayDataException (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayDataException [Невозможно преобразовать String к int] Элемент[" + x + "]" + "[" + y + "]";
+    }
 }
