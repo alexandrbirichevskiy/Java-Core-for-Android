095f738a09a58e0a43e114000b63bc571bac6b1b
Alexandr Birichevskiy <alexandrbirichevskiy@gmail.com>
2021-02-23 23:05:49 +0300

    homework11 v 1.1

---
+++ Homework_11/src/main/java/com/alexbirichevskiy/homework_11/task_2/ArrayToList.java
@@ -0,0 +1,21 @@
+package com.alexbirichevskiy.homework_11.task_2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayToList <T> {
+    private T[] arr;
+
+    public ArrayToList(T[] arr) {
+        this.arr = arr;
+    }
+
+    public List<T> arrayToArrayList() {
+        List <T> list = new ArrayList();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+}
--- Homework_11/src/main/java/com/alexbirichevskiy/homework_11/task_2/Main.java
+++ Homework_11/src/main/java/com/alexbirichevskiy/homework_11/task_2/Main.java
@@ -1,7 +1,12 @@
 package com.alexbirichevskiy.homework_11.task_2;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("l");
+        String[] arrStr = new String[] {"ноль", "один", "два", "три", "четыре"};
+        ArrayToList arr = new ArrayToList(arrStr);
+        System.out.println(arr.arrayToArrayList());
+        System.out.println(arr.arrayToArrayList().getClass());
     }
 }
