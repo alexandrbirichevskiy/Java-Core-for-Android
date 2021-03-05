ecde9f6914cc407acae339e93ae5148f028ef80e
Alexandr Birichevskiy <alexandrbirichevskiy@gmail.com>
2021-03-05 19:48:52 +0300

    homework13 v1.0

---
+++ Homework_13/src/main/java/com/alexbirichevskiy/homework_13/StartRaceMessage.java
@@ -0,0 +1,7 @@
+package com.alexbirichevskiy.homework_13;
+
+public class StartRaceMessage implements Runnable{
+    public void run(){
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+    }
+}
